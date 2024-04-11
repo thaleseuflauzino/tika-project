558e5b782cf8af0cf7bdc298b714063161346ba1
Thales Euflauzino <120137721+thaleseuflauzino@users.noreply.github.com>
2024-04-10 22:28:11 -0300

    adicionado arquivos do tika-project

---
+++ tika_project/tika_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from tika import parser
+
+def extract_text(request):
+    if request.method == 'POST' and request.FILES['file']:
+        file = request.FILES['file']
+        parsed = parser.from_file(file.read())
+        text = parsed['content']
+        return JsonResponse({'text': text})
+    return JsonResponse({'error': 'no file provided'}, status= 400)
