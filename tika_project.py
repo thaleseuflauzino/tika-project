f75e74e1ba79955546414f53184e0f8164eb428b
Thales Euflauzino <thaleseuflauzino@gmail.com>
2024-04-10 23:09:12 -0300

    tst

--- tika_project.py
+++ tika_project.py
@@ -1,20 +1,26 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from tika import parser
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/extract_text', methods=['POST'])
 def extract_text():
-    # Verifica se um arquivo foi enviado na solicitação
     if 'file' not in request.files:
         return jsonify({'error': 'Nenhum arquivo enviado'}), 400
 
     file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'Nenhum arquivo selecionado'}), 400
 
-    # Extrai texto do arquivo usando o Apache Tika
-    raw_text = parser.from_buffer(file).get('content')
-
-    return jsonify({'text': raw_text})
+    try:
+        parsed = parser.from_buffer(file.read())
+        text = parsed['content']
+        return jsonify({'text': text}), 200
+    except Exception as e:
+        print('Erro ao extrair texto:', e)
+        return jsonify({'error': 'Erro ao extrair texto do arquivo'}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
